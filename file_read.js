aa27a7b6792ea47aaf30a313a350d8968d454883
Usman7030 <usman.jamil@blueeast.com>
2022-07-18 15:02:42 +0500

    update code

--- file_read.js
+++ file_read.js
@@ -1,45 +1,63 @@
-const fs = require('fs')
-const path = require('path')
-const _ = require('lodash')
+const fs = require("fs");
+const path = require("path");
+const yaml = require("js-yaml");
+const _ = require("lodash");
 
- var arr3=[]
+module.exports = {
+  parseFile: function (filePath) {
+    let content;
+    let output = new Promise((resolve, reject) => {
+      fs.readFile(filePath, "utf-8", function (err, data) {
+        if (err) reject(err);
+        content = yaml.load(data);
+        resolve(content);
+      });
+    });
+    return output;
+  },
 
- module.exports = {
+  try2: async function () {
+    console.log("start..!");
+    let dir = path.join(__dirname, "./local");
+    let files = await fs.readdirSync(dir); // gives all the files
 
- parseFile:function (filePath) {
-  let content
-  let output = new Promise((resolve, reject) => {
-    fs.readFile(filePath,'utf-8', function(err, data) {
-    if (err) reject( err )
-    content = data.toString().split(/(?:\r\n|\r|\n)/g).map(function(line) {
-      return line.trim()
-    }).filter(Boolean)
-    resolve((content))
-  })
-})
-  return output
-},
+    let promises = await files.map((file) =>
+      module.exports.parseFile(path.join(dir, file))
+    ); // gives an array of promises for each file
+    return [await Promise.all(promises), files];
+  },
+};
 
-try2:async function(){
-    console.log('start................................................');
-// if  (require.main === module) {
-    let dir = path.join(__dirname, './local')
-    console.log(dir);
-    let files = await fs.readdirSync(dir) // gives all the files
-    let promises = await files.map(file => module.exports.parseFile(path.join(dir, file))) // gives an array of promises for each file
-    return await Promise.all(promises)
-
-  
-
-//   }
-}
-
- }
 //  async function main2() {
-//     let abc = await module.exports.try2();
-//     console.log(abc);
+//     let [abc, files] = await module.exports.try2();
+//     console.log( 'bla bla :', files );
+//    console.log( 'bla ok :', await abc );
+
+//    return
+
+//    const tk =yaml.dump(abc[0], {
+//       styles: {
+//         "!!null": "empty", // dump null as ~
+//       },
+//       forceQuotes: true,
+//       sortKeys: true,
+//    } )
+
+//    console.log('tk :',typeof tk);
+
+//   fs.writeFile(
+//     "file4.yaml",
+//     yaml.dump(abc[0], {
+//       styles: {
+//         "!!null": "empty", // dump null as ~
+//       },
+//       forceQuotes: true,
+//       sortKeys: true,
+//     }),
+//     (error) => {
+//       if (error) throw error;
+//     }
+//   );
 
 //   }
-//   main2()
-
-
+// main2();
